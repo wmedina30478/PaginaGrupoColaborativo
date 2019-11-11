ff8d80d263d42454f1d0d709b4e6dde608ac74c4
wmedina30478 <56282060+wmedina30478@users.noreply.github.com>
2019-11-11 15:41:10 -0500

    se realiza actualización de formato para guia 3 usuario: wmedin30478

---
+++ php/cn.php
@@ -0,0 +1,9 @@
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "soluciones_medicas");
+//if(!$conexion){
+//	echo 'Error al conectar a la base de datos';
+//}
+//else{
+//	echo 'Conectado a la base de datos';
+//}
+?>
